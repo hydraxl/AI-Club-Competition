68de461d95cff92cca7681522080b5ac92d509e7
hydraxl <adam.freed123@gmail.com>
2024-03-29 19:26:27 -0700

    Created README for Connect4

--- Connect4/player_functions/example_player.py
+++ Connect4/player_functions/example_player.py
@@ -1,18 +1,3 @@
-### IMPORTANT SYNTAX ###
-# Connect 4 Board is represented as a 2d list
-# Syntax for accessing the value of the board at a given position: board[x][y]
-# x=0 is the far left of the board
-# y=0 is the bottom of the board
-# The board is 7 squares wide and 6 squares tall
-# Your algorithm should return a single integer representing an x coordinate
-#
-# You MUST have a string called "programmer_name"
-# Your algorithm MUST be named "game_player"
-# Your algorithm MUST take in a single input, preferably called "board"
-#
-# All of your code MUST be contained in a single .py file
-
-
 # Name of the programmer
 programmer_name = "Adam Freed"
 
